060239e69ba3c6670dc827867562e8f2850e086a
Christopher Liu <christopherliu@sorcero.com>
2020-10-30 15:05:11 -0400

    Add constructor, size, and add methods

--- SuperArray.java
+++ SuperArray.java
@@ -1,4 +1,22 @@
 public class SuperArray {
     private String[] data;
     private int size;
+
+    public SuperArray() {
+        data = new String[10];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean add(String element) {
+        if (size < data.length) {
+            data[size] = element;
+            size++;
+            return true;
+        }
+        return false;
+    }
 }
